fa3b562fab8ca00adefe62a47f74966494393099
jeremyhilliard14 <jeremyhilliard14@gmail.com>
2016-04-27 22:01:56 -0400

    added get request on page load

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -1,33 +1,61 @@
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
+var mongoUrl = 'mongodb://localhost:27017/btb';
+
+var db;
+//creat a connection to mongo
+MongoClient.connect(mongoUrl, function(error, database){
+	db = database;
+});
 
 /* GET home page. */
+router.get('/search', function(req, res, next){
+	db.collection('students').find().toArray(function(error, studentResult){
+		res.json(studentResult);
+	})
+	//res.json("Welcome to Jeremy's server!");
+});
+
 router.post('/search', function(req, res, next) {
-  	console.log(req.body.name);
+  	// console.log(req.body.name);
+  	//we get nae from the post request(angular)
+  	var individualStudent = req.body.name;
 
-  	var name = req.body.name;
+  	db.collection('students').find({name: individualStudent}).toArray(function(error, studentResult){
 
-  	var dcClass = [
-  		'Tristan',
-  		'Josh',
-  		'Bogdan',
-  		'Keith',
-  		'Will',
-  		'Curtis',
-  		'Joe',
-  		'Kochan',
-  		'Patrick',
-  		'Jonathan',
-  		'Jeremy'
-  	];
+  		if(studentResult.length == 0){
+  			db.collection('students').insertOne({
+  				name: individualStudent
+  			});
+  			res.json("Sorry there were no results. We have added " + individualStudent + ' to the database.')
+  		}else{
+  		res.json(studentResult[0].name + ' is his name.');
+  		}
+  	});
 
 
+  	// var dcClass = [
+  	// 	'Tristan',
+  	// 	'Josh',
+  	// 	'Bogdan',
+  	// 	'Keith',
+  	// 	'Will',
+  	// 	'Curtis',
+  	// 	'Joe',
+  	// 	'Kochan',
+  	// 	'Patrick',
+  	// 	'Jonathan',
+  	// 	'Jeremy'
+  	// ];
 
-  	if(dcClass.indexOf(name) > -1){
-  		res.json({message: "Hello, " + name + ", you are a student in this class."});
-  	}else{
-  		res.json({message: "You are not a student in this class."});
-  	}
+
+
+  	// if(dcClass.indexOf(name) > -1){
+  	// 	res.json({message: "Hello, " + name + ", you are a student in this class."});
+  	// }else{
+  	// 	res.json({message: "You are not a student in this class."});
+  	// }
 
 
 
--- front-end/controller.js
+++ front-end/controller.js
@@ -2,14 +2,32 @@ var searchApp = angular.module('searchApp', []);
 
 searchApp.controller('searchController', function($scope, $http){
 	
-	$scope.search = function(){
-		$scope.message = "Hello, World!!";
+	var apiURL = 'http://localhost:3000/search';
+	//var apiURL = 'http://10.150.41.253:3000/search';
 
-		var apiURL = 'http://localhost:3000/search';
+
+	$http.get(apiURL).then(
+		function successCallback(response){
+			$scope.studentsOnLoad = response.data;
+			console.log($scope.studentsOnLoad);
+					//this callback will be called asynchronously
+				//when the response is available
+		}, function errorCallback(response){
+			$scope.result = "Error!!";
+				//called asynchronously if an error occurs
+				//or server returns response with an error status
+		});
+
+
+	$scope.search = function(){
+		
+		//var apiURL = 'http://localhost:3000/search';
+		var apiURL = 'http://10.150.41.253:3000/search';
+
 
 		$http.post(apiURL, {name: $scope.who}).then(
 			function successCallback(response){
-				$scope.result = response.data.message;
+				$scope.result = response.data;
 				//this callback will be called asynchronously
 				//when the response is available
 			}, function errorCallback(response){
